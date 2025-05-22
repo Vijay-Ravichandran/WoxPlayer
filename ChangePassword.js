a640e67ccc86bba3ee274068a198e669249f02b2
Vijayaragavan R <vijayravi.0906@gmail.com>
2025-05-22 19:36:31 +0530

    Add files via upload

---
+++ ChangePassword.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const ChangePassword = () => {
+  return (
+    <div className="p-4">
+      <h2>Change Password</h2>
+      {/* Form components will go here */}
+    </div>
+  );
+};
+
+export default ChangePassword;
---
+++ EditProfile.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EditProfile = () => {
+  return (
+    <div className="p-4">
+      <h2>Edit Profile</h2>
+      {/* Form components will go here */}
+    </div>
+  );
+};
+
+export default EditProfile;
